b877bb5d8002b20ce0e2db07e4c74444dcda4493
birkan <b.561birkan@gmail.com>
2023-01-18 14:27:18 +0300

    Concrete Views

--- books/api/views.py
+++ books/api/views.py
@@ -1,23 +1,37 @@
-from rest_framework.generics import GenericAPIView
+# from rest_framework.generics import GenericAPIView
+# from ..models import Book, Comment
+# from .serializers import CommentSerializer, BookSerializer
+
+# from rest_framework.mixins import ListModelMixin, CreateModelMixin
+
+# """
+# we import `ListModelMixin` from mixins to get list of all books
+# we import `CreateModelMixin` from mixins to create a new Book instance
+
+# """
+
+# class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Book.objects.all()
+#     serializer_class = BookSerializer
+
+#     # to list all Book instances
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+
+#     # to create a new Book instance
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
 from ..models import Book, Comment
 from .serializers import CommentSerializer, BookSerializer
 
-from rest_framework.mixins import ListModelMixin, CreateModelMixin
+from rest_framework import generics
 
-"""
-we import `ListModelMixin` from mixins to get list of all books
-we import `CreateModelMixin` from mixins to create a new Book instance
-
-"""
-
-class BookListCreateAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
+class BookListCreateAPIView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
-    # to list all Book instances
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    # to create a new Book instance
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
+class BookDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
